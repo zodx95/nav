16c9d5b4f340af9ce23e3a4dcc01152bbcc32687
zodx95 <119392349+zodx95@users.noreply.github.com>
2026-02-10 10:28:23 +0000

    Deploying to gh-pages from @ zodx95/nav@a2e3d0b0ca4d5b375acd2fbae41feca50f4c7b53 🚀

---
+++ chunk-4ZIVM4TD.js
@@ -0,0 +1,1 @@
+import{d as a}from"./chunk-ZFQ2HPAE.js";import{Ab as e,sb as r}from"./chunk-4J7ZBXL6.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
