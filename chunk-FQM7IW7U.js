02da3447cb8c74ce7d65eb0217309d534fcf9d4c
zodx95 <119392349+zodx95@users.noreply.github.com>
2026-02-10 13:59:28 +0000

    Deploying to gh-pages from @ zodx95/nav@d58c5803d9676d3283045c6b41cf071415ef2655 🚀

---
+++ chunk-FQM7IW7U.js
@@ -0,0 +1,1 @@
+import{d as a}from"./chunk-5X3PS2NV.js";import{Ab as e,sb as r}from"./chunk-M443GDWF.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
